38fc5d3f79ec1b446ad812ffb6a11cceb95db153
elinamakarenko <68436291+elinamakarenko@users.noreply.github.com>
2022-03-22 21:59:17 +0200

    second-commit

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+const buttonStart = document.querySelector("button[data-start]");
+const buttonStop = document.querySelector("button[data-stop]");
+let timerId = null;
 
+buttonStart.addEventListener("click", ()=>{
+    
+    timerId=setInterval(()=>{
+      const body = document.querySelector("body");
+      body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+    buttonStart.setAttribute("disabled", "");
+    console.log(buttonStart);
+}
+)
+buttonStop.addEventListener("click", ()=>{
+    clearInterval(timerId);
+    buttonStart.removeAttribute("disabled");
+    console.log(buttonStart);
+})
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
